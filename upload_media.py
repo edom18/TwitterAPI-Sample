d1fec57816d42771bd13da6417bc6e6f9fb5de0b
edom18 <edo.m18@gmail.com>
2024-08-30 07:17:53 +0900

    feat: Upload media 側も Utility を使って Auth header を作るように修正

--- upload_media.py
+++ upload_media.py
@@ -1,71 +1,34 @@
 import os
-import hmac
-import hashlib
-import base64
 import requests
 
-# import http.client as http_client
-# http_client.HTTPConnection.debuglevel = 1
-
-from utility import generate_nonce, get_timestamp, encode_text
+from utility import create_oauth_header
 
 import dotenv
 dotenv.load_dotenv()
 
 def post(files):
 
-    media_upload_endpoint_url = "https://upload.twitter.com/1.1/media/upload.json"
+    endpoint_url = "https://upload.twitter.com/1.1/media/upload.json"
+
     oauth_consumer_key = os.environ.get("CONSUMER_KEY")
     oauth_consumer_secret = os.environ.get("CONSUMER_SECRET")
-    oauth_nonce = generate_nonce()
-    oauth_signature_method = "HMAC-SHA1"
-    oauth_timestamp = str(get_timestamp())
     oauth_token = os.environ.get("AUTH_TOKEN")
     oauth_token_secret = os.environ.get("AUTH_TOKEN_SECRET")
-    oauth_version = "1.0"
 
-    oauth_parameters = {
-        "oauth_consumer_key": oauth_consumer_key,
-        "oauth_token": oauth_token,
-        "oauth_signature_method": oauth_signature_method,
-        "oauth_timestamp": oauth_timestamp,
-        "oauth_nonce": oauth_nonce,
-        "oauth_version": oauth_version,
-    }
-
-    method = "POST"
-
-    # リクエストパラメータ
-    all_parameters = oauth_parameters.copy()
-    sorted_parameters = "&".join(f"{encode_text(k)}={encode_text(v)}" for k, v in sorted(all_parameters.items()))
-
-    print(f"Sorted Parameters: {sorted_parameters}\n")
-
-    # ベースストリングの作成
-    base_string = f"{method}&{encode_text(media_upload_endpoint_url)}&{encode_text(sorted_parameters)}"
-
-    print(f"Base String: {base_string}\n")
-
-    # シグネチャキーの作成
-    signing_key = f"{encode_text(oauth_consumer_secret)}&{encode_text(oauth_token_secret)}"
-
-    # シグネチャの生成
-    signature = base64.b64encode(hmac.new(signing_key.encode(), base_string.encode(), hashlib.sha1).digest()).decode()
-
-    print(f"Signature: {signature}")
-
-    # Authorization ヘッダーの作成
-    oauth_parameters["oauth_signature"] = signature
-    auth_header = "OAuth " + ", ".join([f'{encode_text(k)}="{encode_text(v)}"' for k, v in oauth_parameters.items()])
-
-    print(f"Authorization Header: {auth_header}\n")
+    auth_header = create_oauth_header(
+        endpoint_url=endpoint_url,
+        oauth_consumer_key=oauth_consumer_key,
+        oauth_consumer_secret=oauth_consumer_secret,
+        oauth_token=oauth_token,
+        oauth_token_secret=oauth_token_secret,
+        verbose=False)
 
     # リクエストヘッダーのセット
     headers = {
         "Authorization": auth_header,
     }
 
-    print(f"Access the api [{media_upload_endpoint_url}] ...")
+    print(f"Access the api [{endpoint_url}] ...")
 
-    response = requests.post(media_upload_endpoint_url, headers=headers, files=files)
+    response = requests.post(endpoint_url, headers=headers, files=files)
     return response
