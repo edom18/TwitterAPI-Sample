5f4b15c7038647b56aee3e071957c7063a4f6313
edom18 <edo.m18@gmail.com>
2024-08-30 07:15:46 +0900

    del: コメントアウトを削除

--- post_tweet.py
+++ post_tweet.py
@@ -1,18 +1,10 @@
 import os
-import hmac
-import hashlib
-import base64
 import requests
 
-# import http.client as http_client
-# http_client.HTTPConnection.debuglevel = 1
-
-# from utility import generate_nonce, get_timestamp, encode_text
 from utility import create_oauth_header
 
 import dotenv
 dotenv.load_dotenv()
-
 
 def request(parameters):
 
@@ -22,46 +14,6 @@ def request(parameters):
     oauth_consumer_secret = os.environ.get("CONSUMER_SECRET")
     oauth_token = os.environ.get("AUTH_TOKEN")
     oauth_token_secret = os.environ.get("AUTH_TOKEN_SECRET")
-    # oauth_nonce = generate_nonce()
-    # oauth_signature_method = "HMAC-SHA1"
-    # oauth_timestamp = str(get_timestamp())
-    # oauth_version = "1.0"
-
-    # oauth_parameters = {
-    #     "oauth_consumer_key": oauth_consumer_key,
-    #     "oauth_token": oauth_token,
-    #     "oauth_signature_method": oauth_signature_method,
-    #     "oauth_timestamp": oauth_timestamp,
-    #     "oauth_nonce": oauth_nonce,
-    #     "oauth_version": oauth_version,
-    # }
-
-    # method = "POST"
-
-    # # リクエストパラメータ
-    # all_parameters = oauth_parameters.copy()
-    # sorted_parameters = "&".join(f"{encode_text(k)}={encode_text(v)}" for k, v in sorted(all_parameters.items()))
-
-    # print(f"Sorted Parameters: {sorted_parameters}\n")
-
-    # # ベースストリングの作成
-    # base_string = f"{method}&{encode_text(endpoint_url)}&{encode_text(sorted_parameters)}"
-
-    # print(f"Base String: {base_string}\n")
-
-    # # シグネチャキーの作成
-    # signing_key = f"{encode_text(oauth_consumer_secret)}&{encode_text(oauth_token_secret)}"
-
-    # # シグネチャの生成
-    # signature = base64.b64encode(hmac.new(signing_key.encode(), base_string.encode(), hashlib.sha1).digest()).decode()
-
-    # print(f"Signature: {signature}")
-
-    # # Authorization ヘッダーの作成
-    # oauth_parameters["oauth_signature"] = signature
-    # auth_header = "OAuth " + ", ".join([f'{encode_text(k)}="{encode_text(v)}"' for k, v in oauth_parameters.items()])
-
-    # print(f"Authorization Header: {auth_header}\n")
 
     auth_header = create_oauth_header(
         endpoint_url=endpoint_url,
